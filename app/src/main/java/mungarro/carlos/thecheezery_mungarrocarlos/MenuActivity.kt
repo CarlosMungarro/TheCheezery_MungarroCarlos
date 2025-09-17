0c364c7e1210a3537b59e8b6af58340d23153eb7
CarlosMungarro <carlos.mungarro247590@potros.itson.edu.mx>
2025-09-17 01:33:40 -0600

    Continuacion

--- app/src/main/java/mungarro/carlos/thecheezery_mungarrocarlos/MenuActivity.kt
+++ app/src/main/java/mungarro/carlos/thecheezery_mungarrocarlos/MenuActivity.kt
@@ -15,6 +15,14 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
 
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+
+        btnCold.setOnClickListener {
+
+            var intent: Intent = Intent( this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
